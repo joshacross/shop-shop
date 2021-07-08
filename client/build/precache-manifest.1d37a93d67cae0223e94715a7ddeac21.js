1aa24cea374044514f9a6cbed49291bc7b6dd6ba
Killa J <joshuaalexandercross@gmail.com>
2021-07-07 20:47:22 -0500

    debugging src/utils folder

---
+++ client/build/precache-manifest.1d37a93d67cae0223e94715a7ddeac21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba69f1188f7c593a7e4599e0c0bf753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d139ffd81f68eec26000",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "7a1d3e4e6d8363b93fd0",
+    "url": "/static/js/2.d837444b.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.d837444b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d139ffd81f68eec26000",
+    "url": "/static/js/main.fdb9fdc6.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,75 +1,137 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React, { useEffect } from "react";
+import { useQuery } from '@apollo/react-hooks';
 
-import ProductItem from '../ProductItem';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import spinner from '../../assets/spinner.gif';
-import React, { useEffect } from 'react';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import ProductItem from "../ProductItem";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import spinner from "../../assets/spinner.gif"
 
 function ProductList() {
+  const [state, dispatch] = useStoreContext();
+
+  const { currentCategory } = state;
+
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  useEffect(() => {
+    if(data) {
+      dispatch({
+           type: UPDATE_PRODUCTS,
+          products: data.products
+        });
+    }
+  }, [data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return products;
+      return state.products;
     }
 
-    return products.filter(
-      (product) => product.category._id === currentCategory
-    );
+    return state.products.filter(product => product.category._id === currentCategory);
   }
-
-// retrieve current global state object and the dispatch method to update state 
-const [state, dispatch] = useStoreContext();
-
-// destructure currentCategory data out of the state object to use in the filterProducts() function
-const { currentCategory } = state;
-
-const { loading, data } = useQuery(QUERY_PRODUCTS);
-
-useEffect(() => {
-  if (data) {
-    dispatch({
-      type: UPDATE_PRODUCTS,
-      products: data.products
-    });
-  }
-}, [data, dispatch]);
-
-function filterProducts() {
-  if (!currentCategory) {
-    return state.products;
-  }
-
-  return state.products.filter(product => product.category._id === currentCategory);
-}
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
-          {filterProducts().map((product) => (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
-            />
-          ))}
+            {filterProducts().map(product => (
+                <ProductItem
+                  key= {product._id}
+                  _id={product._id}
+                  image={product.image}
+                  name={product.name}
+                  price={product.price}
+                  quantity={product.quantity}
+                />
+            ))}
         </div>
       ) : (
         <h3>You haven't added any products yet!</h3>
       )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+      { loading ? 
+      <img src={spinner} alt="loading" />: null}
     </div>
   );
 }
 
 export default ProductList;
+
+
+// import React, { useEffect } from 'react';
+// import { useQuery } from '@apollo/client';
+// import ProductItem from '../ProductItem';
+// import { QUERY_PRODUCTS } from '../../utils/queries';
+// import spinner from '../../assets/spinner.gif';
+// import { useStoreContext } from '../../utils/GlobalState';
+// import { UPDATE_PRODUCTS } from '../../utils/actions';
+
+// function ProductList() {
+//   const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+//   const products = data?.products || [];
+
+//   function filterProducts() {
+//     if (!currentCategory) {
+//       return products;
+//     }
+
+//     return products.filter(
+//       (product) => product.category._id === currentCategory
+//     );
+//   }
+
+// // retrieve current global state object and the dispatch method to update state 
+// const [state, dispatch] = useStoreContext();
+
+// // destructure currentCategory data out of the state object to use in the filterProducts() function
+// const { currentCategory } = state;
+
+// const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+// // implement useEffect hook in order to wait for our useQuery() response to come in
+// useEffect(() => {
+//   // once data object returned, execute dispatch function to instruct reducer func that it's the UPDATE_PRODUCTS action
+//   // and should save the array of proudcts data to our global store.
+//   if (data) {
+//     dispatch({
+//       type: UPDATE_PRODUCTS,
+//       products: data.products
+//     });
+//   }
+// }, [data, dispatch]);
+
+// function filterProducts() {
+//   if (!currentCategory) {
+//     return state.products;
+//   }
+
+//   return state.products.filter(product => product.category._id === currentCategory);
+// }
+
+//   return (
+//     <div className="my-2">
+//       <h2>Our Products:</h2>
+//       {products.length ? (
+//         <div className="flex-row">
+//           {filterProducts().map((product) => (
+//             <ProductItem
+//               key={product._id}
+//               _id={product._id}
+//               image={product.image}
+//               name={product.name}
+//               price={product.price}
+//               quantity={product.quantity}
+//             />
+//           ))}
+//         </div>
+//       ) : (
+//         <h3>You haven't added any products yet!</h3>
+//       )}
+//       {loading ? <img src={spinner} alt="loading" /> : null}
+//     </div>
+//   );
+// }
+
+// export default ProductList;
