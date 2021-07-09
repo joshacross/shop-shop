a09c9d2d032854ad63e67d1f1b65e6760849302b
Killa J <joshuaalexandercross@gmail.com>
2021-07-08 22:53:52 -0500

    fixed errors

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,40 +1,19 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import React from 'react';
+import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 
-function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
-
-  const { categories } = state;
-
+function CategoryMenu({ setCategory }) {
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-
-  useEffect(() => {
-    if (categoryData) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories
-      });
-    }
-  }, [categoryData, dispatch]);
-
-  const handleClick = id => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id
-    });
-  };
+  const categories = categoryData?.categories || [];
 
   return (
     <div>
       <h2>Choose a Category:</h2>
-      {categories.map(item => (
+      {categories.map((item) => (
         <button
           key={item._id}
           onClick={() => {
-            handleClick(item._id);
+            setCategory(item._id);
           }}
         >
           {item.name}
@@ -45,63 +24,3 @@ function CategoryMenu() {
 }
 
 export default CategoryMenu;
-
-
-// import React, { useEffect } from 'react';
-// import { useQuery } from '@apollo/client';
-// import { QUERY_CATEGORIES } from '../../utils/queries';
-// import { useStoreContext } from "../../utils/GlobalState";
-// import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
-
-// function CategoryMenu() {
-//   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-//   const categories = categoryData?.categories || [];
-
-//   //retrieve current state from global state object
-//   // use dispact method to update state
-//   const [state, dispatch] = useStoreContext();
-
-//   // destructure categories out of state
-//   const { categories } = state;
-//   // take categoryData that returns from the userQuery() hook and use dispatch method to set global state
-//   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-
-//   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  
-//   // when component loads and response from useQuery, useEffect notices that categoryData is not undefined and runs dispatch function, setting category data to the global state
-//   useEffect(() => {
-//     // if categoryData exists or has changed from the response of useQuery, then run dispatch()
-//     if (categoryData) {
-//       // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
-//       dispatch({
-//         type: UPDATE_CATEGORIES,
-//         categories: categoryData.categories
-//       });
-//     }
-//   }, [categoryData, dispatch]);
-
-//   const handleClick = id => {
-//     dispatch({
-//       type: UPDATE_CURRENT_CATEGORY,
-//       currentCategory: id
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <h2>Choose a Category:</h2>
-//       {categories.map(item => (
-//         <button
-//           key={item._id}
-//           onClick={() => {
-//             handleClick(item._id);
-//           }}
-//         >
-//           {item.name}
-//         </button>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default CategoryMenu;
