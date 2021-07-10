a7ef240f3c03377f997870109b31b6235b83ac95
Killa J <joshuaalexandercross@gmail.com>
2021-07-10 00:13:11 -0500

    initialized indexDB helper function

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,3 +4,29 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
+
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    // open connection to the db 'shop-shop' with the version of 1
+    const request = window.indexedDB.open('shop-shop', 1);
+
+    // create variables to hold reference to the db, transaction (tx), and obj store
+    let db, tx, store;
+
+    // if version has changed or first use, run method & create 3 obj stores
+    request.onupgradeneeded = function(e) {
+      const db = request.result;
+
+      // create obj store for each type of data and set 'primary'
+      db.createObjectStore('products', { keyPath: '_id'});
+      db.createObjectStore('categories', { keyPath: '_id'});
+      db.createObjectStore('cart', { keyPath: '_id'});
+    };
+
+    // handle any err with connect
+    request.onerror = function(e) {
+      console.log('There was an error');
+    };
+
+  });
+}
