55de41e38abc29caa97df419144464695b01fdde
Killa J <joshuaalexandercross@gmail.com>
2021-07-09 23:35:06 -0500

    finalized cart-ui with CRUD cart items

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 // expects item component as a prop and will use that object's properties to populate JSX
 const CartItem = ({ item }) => {
+
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = item => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id
+    });
+  };
+
+  const onChange = (e) => {
+    const value = e.target.value;
+  
+    if (value === '0') {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value)
+      });
+    }
+  };
+
   return (
     <div className="flex-row">
       <div>
@@ -18,10 +47,12 @@ const CartItem = ({ item }) => {
             type="number"
             placeholder="1"
             value={item.purchaseQuantity}
+            onChange={onChange}
           />
           <span
             role="img"
             aria-label="trash"
+            onClick={() => removeFromCart(item)}
           >
             🗑️
           </span>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,7 @@ import Cart from '../components/Cart';
 const Home = () => {
   return (
     <div className="container">
+      <Cart />
       <CategoryMenu />
       <ProductList />
     </div>
